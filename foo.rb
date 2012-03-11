dbc8d63828675027e4dbb2661ff2d93925061739
Sosloow <sosloow@gmail.com>
2012-03-11 21:44:55 +0600

    started with layout

--- foo.rb
+++ foo.rb
@@ -1,10 +1,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
-  
- get '/' do
-   haml :index
- end
+
+enable :sessions
+set :session_secret, 'secret'
+
+get '/' do
+  haml :index
+end
 
 after do
   puts response.status
