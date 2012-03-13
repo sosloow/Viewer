bc5d2bbe92dfbf24b6b1c96b41804e83709ec668
Sosloow <sosloow@gmail.com>
2012-03-13 15:22:58 +0600

    finished with mongo (I hope)

--- models.rb
+++ models.rb
@@ -1,9 +1,17 @@
 require 'mongo_mapper'
 
+#uri =  URI.parse(ENV['MONGOHQ_URL'])
+#mongo_connection = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+#MongoMapper.database = 'viewer'
+#p uri.user, uri.password
+#MongoMapper.database.authenticate('sosloow', 'finntroll') 
+MongoMapper.config = { 
+  'dev' => { 'uri' => ENV['MONGOHQ_URL'] } }
+MongoMapper.connect('dev')
+
 class Comic
 
     include MongoMapper::Document
-    set_database_name "veiwer-dev"
 
     key :title, String, required: true
     key :images, Array, required: true
